f7dd7443d4a418fa1a31543c79c9ffda023243be
Andrepinheiroo <andrepinheiro.code@gmail.com>
2024-07-11 09:22:19 -0300

    Create colecoes_dicionario.py

---
+++ Cibersegurança 2024/Fundamentos JS & python/colecoes_dicionario.py
@@ -0,0 +1,12 @@
+#Percorrendo um dicionário: 
+
+
+for exemplar, num_exemplar in coleta.items():
+  print(f'Exemplar: {exemplar}, número de exemplares coletados: {num_exemplar}')
+
+# Saída: 
+# Exemplar: Ford Mustang, número de exemplares coletados: 3
+# Exemplar: Chevrolet Camaro, número de exemplares coletados: 5
+# Exemplar: Dodge Charger, número de exemplares coletados: 2
+# Exemplar: Porsche 911, número de exemplares coletados: 4
+# Exemplar: Lamborghini Huracan, número de exemplares coletados: 6
